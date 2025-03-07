bedbd24ead2b1612a6bf3e5c7888a5a74c757573
Jon Whittingham <jon@crownparkcomputing.com>
2025-03-07 17:10:31 +0000

    Fix Android V2 embedding and update dependencies

---
+++ android/app/src/main/kotlin/com/example/game_launcher/FlutterApp.kt
@@ -0,0 +1,11 @@
+package com.example.game_launcher
+
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.app.FlutterApplication
+
+class FlutterApp : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+} 
